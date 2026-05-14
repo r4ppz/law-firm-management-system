c5077443a0cfe862a680c2aa2b3b3273ec94f3dc
r4ppz <johnreyrabosa.f@gmail.com>
2026-05-14 10:25:53 +0800

    feat: install storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+import storybook from "eslint-plugin-storybook";
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
@@ -13,6 +14,7 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  ...storybook.configs["flat/recommended"],
 ]);
 
 export default eslintConfig;
