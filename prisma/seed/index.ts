8929d3a63e0147aa93c5b21fc60db4f7f587df9d
r4ppz <johnreyrabosa.f@gmail.com>
2026-06-20 12:21:34 +0800

    chore(prisma): remove other seeds except user

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -1,21 +1,9 @@
 import { prisma } from "@/lib/prisma";
 
-import { seedAssignments } from "./seed-assignments";
-import { seedCases } from "./seed-cases";
-import { seedClients } from "./seed-clients";
-import { seedConsultations } from "./seed-consultations";
-import { seedMilestones } from "./seed-milestones";
-import { seedNotifications } from "./seed-notifications";
 import { seedUsers } from "./seed-users";
 
 async function main() {
   await seedUsers();
-  await seedClients();
-  await seedConsultations();
-  await seedCases();
-  await seedAssignments();
-  await seedMilestones();
-  await seedNotifications();
 }
 
 main()
